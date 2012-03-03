c24b94df079cc745fc3939424e0a71980a25ddc0
Conrad Dean <cdean@cpaglobal.com>
2012-03-03 13:43:33 -0600

    cleanup with jquery idioms and variable declarations

--- magic.js
+++ magic.js
@@ -1,23 +1,21 @@
 $(document).ready(function(){
   $.fn.extend({
     menuIn: function(){
-              var obj = $(this);
               var root = $("#top");
               
-              obj.show().width(root.width());
               var y = root.offset().top;
               var xEntry = root.offset().left + root.width();
               var x = root.offset().left;
 
-              obj.offset({top:y,left:xEntry});
-              obj.animate({left:0});
+              this.show().width(root.width());
+              this.offset({top:y,left:xEntry});
+              this.animate({left:0});
 
             },
     menuOut: function(){
-              var obj = $(this);
               var root = $("#top");
               var xEntry = root.offset().left + root.width();
-              obj.animate({left:xEntry});
+              this.animate({left:xEntry});
             }
   });
 
