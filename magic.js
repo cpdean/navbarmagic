ba49f841c35a4457a38503a2a102eae2687a4a4d
Conrad Dean <cdean@cpaglobal.com>
2012-03-03 12:53:08 -0600

    more solid prototype than earlier today

--- magic.js
+++ magic.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 
   $("#oneLink").click(function(){
     $("#one").show();
+
+    var y = $("#top").offset().top;
+    var xEntry = $("#top").offset().left + $("#top").width();
+    var x = $("#top").offset().left;
+
+    $("#one").offset({top:y,left:xEntry});
+    $("#one").animate({left:0});
   });
 
   $("#twoLink").click(function(){
@@ -11,5 +18,11 @@ $(document).ready(function(){
   $(".back").click(function(){
     $(this).parent("ul").hide();
   });
+
+  // set to fixed width
+  var w = $("div#prototype").width();
+  var h = $("div#prototype").height();
+  $("div#prototype").width(w);
+  $("div#prototype").height(h);
 
 });
